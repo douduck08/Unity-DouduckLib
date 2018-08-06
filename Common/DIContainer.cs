ab19310918a777fcb641c629326cca75c65e4501
douduck08 <lu10082000@gmail.com>
2018-08-06 23:13:39 +0800

    add service locator and di container

---
+++ Common/DIContainer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace DouduckLib {
+    public class DIContainer {
+        readonly IDictionary<Type, Type> types = new Dictionary<Type, Type> ();
+        readonly IDictionary<Type, object> typeInstances = new Dictionary<Type, object> ();
+
+        public void Register<TContract, TImplementation> () {
+            types[typeof (TContract)] = typeof (TImplementation);
+        }
+
+        public void Register<TContract, TImplementation> (TImplementation instance) {
+            typeInstances[typeof (TContract)] = instance;
+        }
+
+        public T Resolve<T> () {
+            return (T) Resolve (typeof (T));
+        }
+
+        public object Resolve (Type contract) {
+            if (typeInstances.ContainsKey (contract)) {
+                return typeInstances[contract];
+            } else {
+                Type implementation = types[contract];
+                ConstructorInfo constructor = implementation.GetConstructors () [0];
+                ParameterInfo[] constructorParameters = constructor.GetParameters ();
+                if (constructorParameters.Length == 0) {
+                    return Activator.CreateInstance (implementation);
+                }
+                List<object> parameters = new List<object> (constructorParameters.Length);
+                foreach (ParameterInfo parameterInfo in constructorParameters) {
+                    parameters.Add (Resolve (parameterInfo.ParameterType));
+                }
+                return constructor.Invoke (parameters.ToArray ());
+            }
+        }
+    }
+}
---
+++ Common/ServiceLocator/IUpdatable.cs
@@ -0,0 +1,13 @@
+﻿namespace DouduckLib {
+    public interface IUpdatable {
+        void OnUpdate ();
+    }
+
+    public interface IFixedUpdatable {
+        void OnFixedUpdate ();
+    }
+
+    public interface ILateUpdatable {
+        void OnLateUpdate ();
+    }
+}
