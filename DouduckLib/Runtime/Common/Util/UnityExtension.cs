268c9b062cb8c22a2d65a9fbaf02958bfdd5ca65
douduck08 <lu10082000@gmail.com>
2019-01-06 23:49:29 +0800

    fix namespace of UnityExtension

--- DouduckLib/Runtime/Common/Util/UnityExtension.cs
+++ DouduckLib/Runtime/Common/Util/UnityExtension.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public static class UnityExtension {
-    public static T GetOrAddComponent<T> (this Component trans) where T : Component {
-        T result = trans.GetComponent<T> ();
-        if (result == null) {
-            result = trans.gameObject.AddComponent<T> ();
+namespace DouduckLib {
+    public static class UnityExtension {
+        public static T GetOrAddComponent<T> (this Component trans) where T : Component {
+            T result = trans.GetComponent<T> ();
+            if (result == null) {
+                result = trans.gameObject.AddComponent<T> ();
+            }
+            return result;
         }
-        return result;
-    }
 
 #if UNITY_5_3
-    public static T Instantiate<T> (this GameObject go, GameObject origin, Transform parent) where T : GameObject {
-        T go_ = GameObject.Instantiate<T> (origin);
-        go_.transform.SetParent (parent);
-    }
+        public static T Instantiate<T> (this GameObject go, GameObject origin, Transform parent) where T : GameObject {
+            T go_ = GameObject.Instantiate<T> (origin);
+            go_.transform.SetParent (parent);
+        }
 #endif
+    }
 }
