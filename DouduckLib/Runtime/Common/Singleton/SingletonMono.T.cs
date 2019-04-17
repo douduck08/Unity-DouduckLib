4de2e2b272c00ecd6f639e836b0498e6e79dbe22
douduck08 <lu10082000@gmail.com>
2019-04-17 21:19:03 +0800

    Update SingletonMono

--- DouduckLib/Runtime/Common/Singleton/SingletonMono.T.cs
+++ DouduckLib/Runtime/Common/Singleton/SingletonMono.T.cs
@@ -6,55 +6,53 @@ namespace DouduckLib {
         private static T _instance;
         private static GameObject _gameObject;
         private static object _lock = new object ();
-
-        protected SingletonMono () { }
+        private static bool _dontDestroyOnLoad = false;
+        private static bool _applicationIsQuitting = false;
 
         public static T instance {
             get {
-                if (applicationIsQuitting) {
+                if (_applicationIsQuitting) {
                     return null;
                 }
 
                 lock (_lock) {
-                    if (_instance == null) {
-                        var result = FindObjectsOfType (typeof (T));
-                        if (result.Length > 1) {
-                            throw new InvalidOperationException ("[Singleton] There should never be more than 1 singleton!");
-                        } else if (result.Length == 1) {
-                            _instance = (T) result[0];
-                        }
-
-                        if (_instance == null) {
-                            var go = new GameObject ("[Singleton]" + typeof (T).Name);
-                            var component = go.AddComponent<T> ();
-                            InitializeSingletonMono (component);
-                        }
-                    }
                     return _instance;
                 }
             }
         }
 
-        protected static void InitializeSingletonMono (T component, bool dontDestroyOnLoad = true) {
-            if (_instance != null) {
-                if (_instance != component)
-                    throw new InvalidOperationException ("[Singleton] There should never be more than 1 singleton!");
-                else
-                    return;
+        protected virtual void OnSingletonAwake () { }
+        protected virtual void OnSingletonDestroy () { }
+
+        protected virtual void Awake () {
+            if (_instance == null) {
+                _instance = this as T;
+                _gameObject = this.gameObject;
+                OnSingletonAwake ();
+            } else if (_instance != this) {
+                if (_dontDestroyOnLoad) {
+                    Debug.Log ("[Singleton] An instance of " + typeof (T) + "has created with DontDestroyOnLoad. Destroy the new one.");
+                    Destroy (this);
+                } else {
+                    throw new InvalidOperationException ("[Singleton] There should never be more than 1 singleton instance of " + typeof (T));
+                }
             }
-            _instance = component;
-            _gameObject = component.gameObject;
-            if (dontDestroyOnLoad) DontDestroyOnLoad (_gameObject);
         }
 
-        private static bool applicationIsQuitting = false;
-        protected virtual void Awake () {
-            applicationIsQuitting = false;
+        protected void MarkAsCrossSceneSingleton () {
+            _dontDestroyOnLoad = true;
+            DontDestroyOnLoad (_gameObject);
+            Debug.Log ("[Singleton] An instance of " + typeof (T) + "was marked as DontDestroyOnLoad.");
         }
 
         protected void OnDestroy () {
-            _instance = null;
-            applicationIsQuitting = true;
+            if (_dontDestroyOnLoad) {
+                _applicationIsQuitting = true;
+            } else {
+                _instance = null;
+                _gameObject = null;
+            }
+            OnSingletonDestroy ();
         }
     }
 }
