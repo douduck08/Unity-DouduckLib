d66e138a5e774711791cf7ac8d6df31ddab675f6
douduck08 <lu10082000@gmail.com>
2019-07-10 14:11:51 +0800

    Add CameraAlignTool

---
+++ DouduckLib/Editor/Toolkit/CameraAlignTool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace DouduckLibEditor {
+    [InitializeOnLoad]
+    public class CameraAlignTool {
+
+        static CameraAlignTool () {
+            EditorApplication.update += Update;
+        }
+
+        static void Update () {
+            if (alignToggle) {
+                AlignMainCameraWithScene ();
+            }
+        }
+
+        static bool alignToggle;
+        const string path = "Edit/Keep Aligning Main Camera With Scene View";
+
+        [MenuItem (path, false, 21)]
+        static void ToggleAlignMainCameraWithScene () {
+            alignToggle = !alignToggle;
+            Menu.SetChecked (path, alignToggle);
+        }
+
+        [MenuItem ("Edit/Align Main Camera With Scene View", false, 22)]
+        static void AlignMainCameraWithScene () {
+            var sceneViewCamera = SceneView.lastActiveSceneView.camera;
+            var mainCamera = Camera.main;
+            if (mainCamera != null) {
+                mainCamera.transform.position = sceneViewCamera.transform.position;
+                mainCamera.transform.rotation = sceneViewCamera.transform.rotation;
+            }
+        }
+
+        [MenuItem ("Edit/Align Scene Camera With Game View", false, 23)]
+        static void AlignSceneCameraWithGame () {
+            var sceneView = SceneView.lastActiveSceneView;
+            sceneView.orthographic = Camera.main.orthographic;
+            sceneView.LookAtDirect (Camera.main.transform.position, Camera.main.transform.rotation, 0);
+        }
+    }
+}
