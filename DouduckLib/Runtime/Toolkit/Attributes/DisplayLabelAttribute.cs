c23e8a801507bcbf1fde64e79119a1ae0849a1d8
douduck08 <lu10082000@gmail.com>
2019-07-28 02:23:50 +0800

    Add DisplayLabelAttribute

---
+++ DouduckLib/Runtime/Toolkit/Attributes/DisplayLabelAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+namespace DouduckLib {
+    [AttributeUsage (AttributeTargets.Field)]
+    public class DisplayLabelAttribute : PropertyAttribute {
+        public string label;
+        public DisplayLabelAttribute (string label) {
+            this.label = label;
+        }
+    }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer (typeof (DisplayLabelAttribute))]
+    public class DisplayLabelPropertyDrawer : PropertyDrawer {
+        public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
+            var displayLabelAttribute = attribute as DisplayLabelAttribute;
+            label.text = displayLabelAttribute.label;
+            EditorGUI.PropertyField (position, property, label, true);
+        }
+    }
+#endif
+}
