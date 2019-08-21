fa6bc44439c758eef3b698400f2477ac5aa37f5b
douduck08 <lu10082000@gmail.com>
2019-08-21 19:53:32 +0800

    Update SceneRequester

--- DouduckLib/Runtime/Additional/SceneRequester.cs
+++ DouduckLib/Runtime/Additional/SceneRequester.cs
@@ -1,14 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace DouduckLib {
     public class SceneRequester : MonoBehaviour {
+
         [SerializeField, SceneAsset] List<string> sceneAssets;
+        bool loadOnStart;
 
         void Start () {
-            for (int i = 0; i < sceneAssets.Count; i++) {
-                new SceneLoader (sceneAssets[i], true).StartLoading (this);
+            if (loadOnStart) {
+                LoadAllSceneInList ();
+            }
+        }
+
+        public void LoadAllSceneInList () {
+            var scenesToLoad = new List<string> (sceneAssets);
+            foreach (var scene in UnityUtil.GetAllLoadedScenes ()) {
+                scenesToLoad.Remove (scene.path);
+            }
+            for (int i = 0; i < scenesToLoad.Count; i++) {
+                new SceneLoader (scenesToLoad[i], true).StartLoading (this);
+            }
+        }
+
+        public void UnloadSceneOutOfList () {
+            var scenesToUnload = UnityUtil.GetAllLoadedScenes ().Select (p => p.path).ToList ();
+            foreach (var scenePath in sceneAssets) {
+                scenesToUnload.Remove (scenePath);
+            }
+            for (int i = 0; i < scenesToUnload.Count; i++) {
+                new SceneLoader ("", scenesToUnload[i]).StartLoading (this);
             }
         }
     }
