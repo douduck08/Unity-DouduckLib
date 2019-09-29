3f58846ce52b4f4c48ebe7a984abaae07a7ba3d4
douduck08 <lu10082000@gmail.com>
2019-09-30 02:26:59 +0800

    Update CoroutineCarrier

--- DouduckLib/Runtime/Common/CoroutineCarrier.cs
+++ DouduckLib/Runtime/Common/CoroutineCarrier.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +11,31 @@ namespace DouduckLib {
 
         public static void StartCoroutineOnDontDestroy (IEnumerator routine) {
             instance.StartCoroutine (routine);
+        }
+
+        public static void RunDelaySeconds (Action callback, float seconds) {
+            if (callback != null) {
+                instance.StartCoroutine (DelaySeconds (callback, seconds));
+            }
+        }
+
+        static IEnumerator DelaySeconds (Action callback, float seconds) {
+            yield return new WaitForSeconds (seconds);
+            callback.Invoke ();
+        }
+
+        public static void RunDelayFrames (Action callback, int frames) {
+            if (callback != null) {
+                instance.StartCoroutine (DelaySeconds (callback, frames));
+            }
+        }
+
+        static IEnumerator DelayFrames (Action callback, int frames) {
+            while (frames > 0) {
+                frames -= 1;
+                yield return null;
+            }
+            callback.Invoke ();
         }
     }
 }
