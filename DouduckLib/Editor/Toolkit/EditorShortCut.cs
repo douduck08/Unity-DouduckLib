b16297c38ad94de71057fea90fcda728e7f5d92e
douduck08 <lu10082000@gmail.com>
2019-12-04 13:20:19 +0800

    Update EditorShortCut

--- DouduckLib/Editor/Toolkit/EditorShortCut.cs
+++ DouduckLib/Editor/Toolkit/EditorShortCut.cs
@@ -5,19 +5,35 @@ namespace DouduckLibEditor {
     public class EditorShortCut {
 
         [MenuItem ("Edit/Lock %l")]
-        public static void LockInspector () {
+        static void LockInspector () {
             ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
             ActiveEditorTracker.sharedTracker.ForceRebuild ();
         }
 
         [MenuItem ("Edit/Lock %l", true)]
-        public static bool LockInspectorValid () {
+        static bool LockInspectorValid () {
             return ActiveEditorTracker.sharedTracker.activeEditors.Length != 0;
         }
 
         [MenuItem ("Edit/Unselect All %#a", false, 20)]
-        public static void Unselect () {
+        static void Unselect () {
             Selection.activeGameObject = null;
+        }
+
+        [MenuItem ("GameObject/Group Selected %g")]
+        static void GroupSelected () {
+            if (Selection.activeTransform == null) {
+                return;
+            }
+
+            var go = new GameObject () { name = "Group" };
+            go.transform.SetParent (Selection.activeTransform.parent, false);
+            Undo.RegisterCreatedObjectUndo (go, "Group object");
+
+            foreach (var transform in Selection.transforms) {
+                Undo.SetTransformParent (transform, go.transform, "Group Selected");
+            }
+            Selection.activeObject = go;
         }
     }
 }
