284c7bf8bde7d6e93ed58d83f8f51c02c9704753
douduck08 <lu10082000@gmail.com>
2020-02-10 16:40:01 +0800

    update SceneSaveProcessor

--- DouduckLib/Runtime/Toolkit/SceneSaveProcessor.cs
+++ DouduckLib/Runtime/Toolkit/SceneSaveProcessor.cs
@@ -9,17 +9,15 @@ namespace DouduckLib {
     }
 
 #if UNITY_EDITOR
-    public class SceneSaveProcessor : UnityEditor.AssetModificationProcessor {
-        static string[] OnWillSaveAssets (string[] paths) {
-            foreach (string path in paths) {
+    public class SceneSaveProcessor : UnityEditor.AssetPostprocessor {
+        static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) {
+            foreach (string path in importedAssets) {
                 if (Path.GetExtension (path).Equals (".unity")) {
                     foreach (Transform trans in UnityUtil.GetAllTransforms ()) {
                         OnSceneSaveProcessing (trans);
                     }
-                    return paths;
                 }
             }
-            return paths;
         }
 
         private static void OnSceneSaveProcessing (Transform trans) {
