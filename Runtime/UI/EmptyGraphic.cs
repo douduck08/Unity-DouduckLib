c73c297750d0c6a3868de0db4791952d5ec9075d
douduck08 <lu10082000@gmail.com>
2023-03-20 00:41:51 +0800

    rename EmptyUI to EmptyGraphic

---
+++ Runtime/UI/EmptyGraphic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DouduckLib.UI
+{
+    [RequireComponent(typeof(CanvasRenderer))]
+    public class EmptyGraphic : MaskableGraphic
+    {
+        protected EmptyGraphic()
+        {
+            useLegacyMeshGeneration = false;
+        }
+        protected override void OnPopulateMesh(VertexHelper toFill)
+        {
+            toFill.Clear();
+        }
+    }
+}
