7da13a6e8d86c1eb51004a2fc9206ab525e7cde4
douduck08 <lu10082000@gmail.com>
2023-03-20 00:43:11 +0800

    fix compile error in MoreUIMenuItems

--- Editor/UI/MoreUIMenuItems.cs
+++ Editor/UI/MoreUIMenuItems.cs
@@ -3,42 +3,48 @@ using UnityEngine.UI;
 using UnityEditor;
 using DouduckLib.UI;
 
-namespace DouduckLibEditor.UI {
-    public class MoreUIMenuItems {
+namespace DouduckLibEditor.UI
+{
+    public class MoreUIMenuItems
+    {
         const string uiMenuItemPath = "GameObject/UI/Others/";
 
-        [MenuItem (uiMenuItemPath + "Empty UI")]
-        static void CreateEmptyUI (MenuCommand command) {
-            var go = new GameObject ("EmptyUI");
-            go.AddComponent<EmptyUI> ();
-            GameObjectUtility.SetParentAndAlign (go, command.context as GameObject);
-            Undo.RegisterCreatedObjectUndo (go, "Create" + go.name);
+        [MenuItem(uiMenuItemPath + "Empty Graphic")]
+        static void CreateEmptyGraphic(MenuCommand command)
+        {
+            var go = new GameObject("EmptyGraphic");
+            go.AddComponent<EmptyGraphic>();
+            GameObjectUtility.SetParentAndAlign(go, command.context as GameObject);
+            Undo.RegisterCreatedObjectUndo(go, "Create" + go.name);
             Selection.activeObject = go;
         }
 
-        [MenuItem (uiMenuItemPath + "Interactable Text")]
-        static void CreateInteractableText (MenuCommand command) {
-            var go = new GameObject ("InteractableText");
-            go.AddComponent<InteractableText> ();
-            GameObjectUtility.SetParentAndAlign (go, command.context as GameObject);
-            Undo.RegisterCreatedObjectUndo (go, "Create" + go.name);
+        [MenuItem(uiMenuItemPath + "Interactable Text")]
+        static void CreateInteractableText(MenuCommand command)
+        {
+            var go = new GameObject("InteractableText");
+            go.AddComponent<InteractableText>();
+            GameObjectUtility.SetParentAndAlign(go, command.context as GameObject);
+            Undo.RegisterCreatedObjectUndo(go, "Create" + go.name);
             Selection.activeObject = go;
         }
 
-        [MenuItem (uiMenuItemPath + "Non-Raycast Image")]
-        static void CreatImage (MenuCommand menuCommand) {
-            EditorApplication.ExecuteMenuItem ("GameObject/UI/Image");
+        [MenuItem(uiMenuItemPath + "Non-Raycast Image")]
+        static void CreatImage(MenuCommand menuCommand)
+        {
+            EditorApplication.ExecuteMenuItem("GameObject/UI/Image");
             GameObject go = Selection.activeGameObject;
-            GameObjectUtility.SetParentAndAlign (go, menuCommand.context as GameObject);
-            go.GetComponent<Image> ().raycastTarget = false;
+            GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
+            go.GetComponent<Image>().raycastTarget = false;
         }
 
-        [MenuItem (uiMenuItemPath + "Non-Raycast Text")]
-        static void CreatText (MenuCommand menuCommand) {
-            EditorApplication.ExecuteMenuItem ("GameObject/UI/Text");
+        [MenuItem(uiMenuItemPath + "Non-Raycast Text")]
+        static void CreatText(MenuCommand menuCommand)
+        {
+            EditorApplication.ExecuteMenuItem("GameObject/UI/Text");
             GameObject go = Selection.activeGameObject;
-            GameObjectUtility.SetParentAndAlign (go, menuCommand.context as GameObject);
-            go.GetComponent<Text> ().raycastTarget = false;
+            GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
+            go.GetComponent<Text>().raycastTarget = false;
         }
     }
 }
