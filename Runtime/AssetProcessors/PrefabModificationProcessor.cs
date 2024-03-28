b0c76f8124f905d7cbce19e7bb8fd7ce70d4cecd
douduck08 <lu10082000@gmail.com>
2024-03-29 02:48:14 +0800

    update the prefab asset processor

---
+++ Runtime/AssetProcessors/PrefabModificationProcessor.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
+
+namespace DouduckLib
+{
+    public interface IPrefabSaveProcessing
+    {
+        // void OnPrefabApply();
+        void OnPrefabSave();
+    }
+
+#if UNITY_EDITOR
+    public class PrefabModificationProcessor : AssetModificationProcessor
+    {
+        static string[] OnWillSaveAssets(string[] paths)
+        {
+            var prefabStage = PrefabStageUtility.GetCurrentPrefabStage();
+            var root = prefabStage?.prefabContentsRoot;
+            if (root != null)
+            {
+                OnPrefabSave(root);
+            }
+            else
+            {
+                foreach (string path in paths)
+                {
+                    if (Path.GetExtension(path).Equals(".prefab"))
+                    {
+                        var go = LoadPrefab(path);
+                        if (go != null)
+                        {
+                            OnPrefabSave(go);
+                        }
+                    }
+                }
+            }
+            return paths;
+        }
+
+        static GameObject LoadPrefab(string path)
+        {
+            var go = AssetDatabase.LoadAssetAtPath(path, typeof(GameObject)) as GameObject;
+            return go;
+        }
+
+        static void OnPrefabSave(GameObject go)
+        {
+            Component[] components = go.GetComponentsInChildren<Component>(true);
+            foreach (Component component in components)
+            {
+                if (component is IPrefabSaveProcessing)
+                {
+                    (component as IPrefabSaveProcessing).OnPrefabSave();
+                }
+            }
+        }
+    }
+#endif
+}
